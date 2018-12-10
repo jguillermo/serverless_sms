dd3f2e0ef87707f2980da9a5c07a72cf85e0333f
J Guillermo <jguillermo@lacafetalab.pe>
2018-12-10 12:06:24 -0500

    feat(msm): envio de mensajes de texto

---
+++ handler.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var AWS = require('aws-sdk');
+
+async function enviar_sms(message,phone) {
+          AWS.config.update({ region: 'us-east-1' });
+          // Create publish parameters
+          var params = {
+            Message: 'hola pepe', /* required */
+            PhoneNumber: '+051951737296',
+          };
+          console.log(params);
+          // Create promise and SNS service object
+          var publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' }).publish(params).promise();
+
+          // handle promise's fulfilled/rejected states
+          await publishTextPromise.then(
+            function (data) {
+              console.log("MessageID is " + data.MessageId);
+            }).catch(
+              function (err) {
+                console.error(err, err.stack);
+                throw new Error('error en el servidor');
+              });
+}
+
+
+module.exports.hello = async (event, context) => {
+
+  let statusCode_number=200;
+  let message_txt='Ok';
+
+  try{
+    await enviar_sms();
+  }catch(error){
+    statusCode_number=500;
+     message_txt='error en el servidor';
+  }
+  
+  return {
+    statusCode:statusCode_number,
+    body: JSON.stringify({
+      message:message_txt,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
